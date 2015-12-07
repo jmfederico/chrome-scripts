4f458f4f170413898c52e8daaaa67c2dc2b91595
Federco Jaramillo Martínez <federicojaramillom@gmail.com>
2015-12-07 16:00:29 -0500

    Redirect to BancoDeBogotá

---
+++ BancodeBogotá/README.md
@@ -0,0 +1,2 @@
+# Banco de Bogotá twiks
+My other bank (Banco de Bogotá) thinks security is forcing me to visit their home page before login in. This script redirects from their homepage to the login screen, so that I can get a session id.
---
+++ BancodeBogotá/bancodebogota.redirect.js
@@ -0,0 +1,8 @@
+/* jshint node: true */
+'use strict';
+
+if (window.location.hash === '#fjm') {
+    window.onload = function () {
+        window.location = 'https://www.bancodebogota.com/Banking/pb/exterior/logonUniversalBog?s';
+    };
+}
