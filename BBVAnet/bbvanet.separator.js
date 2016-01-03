31a7ccb2510ef780158aa8095f34f48cd6446df7
Federco Jaramillo Martínez <federicojaramillom@gmail.com>
2016-01-03 12:30:39 -0500

    Better regex match, finish at 2 digits after final dot.

--- BBVAnet/bbvanet.separator.js
+++ BBVAnet/bbvanet.separator.js
@@ -10,7 +10,7 @@
     $('.pesetas:not(:has(*)), .pesetas-i:not(:has(*))').each(function () {
         var $this = $(this);
 
-        if ($this.html().match(/[0-9]{1,3}\.[0-9]{2}/) !== null) {
+        if ($this.html().match(/[0-9]{1,3}\.[0-9]{2}$/) !== null) {
             numeral.language('en');
             var valor = numeral($this.html());
             numeral.language('es');
