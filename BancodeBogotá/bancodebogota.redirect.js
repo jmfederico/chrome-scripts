966898a736d8e04d56a5f3c4e1fe959d6304aa01
Federco Jaramillo Martínez <federicojaramillom@gmail.com>
2016-02-17 13:29:25 -0500

    Remove BancodeBogotá and TeuxDeux, I no longer need them.

--- BancodeBogotá/bancodebogota.redirect.js
+++
@@ -1,8 +0,0 @@
-/* jshint node: true */
-'use strict';
-
-if (window.location.hash === '#fjm') {
-    window.onload = function () {
-        window.location = 'https://www.bancodebogota.com/Banking/pb/exterior/logonUniversalBog?s';
-    };
-}
--- teuxdeux/README.md
+++
@@ -1,3 +0,0 @@
-# TeuxDeux app
-This extension creates a link to teuxdeux.com as an app.
-You can add it to your Dock, or launch it from Finder.
